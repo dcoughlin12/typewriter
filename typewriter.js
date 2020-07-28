1bd4fbbaebacb42e75f9af2b86d02767ce7f90b1
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-28 19:14:35 +0000

    completed challenge

--- typewriter.js
+++ typewriter.js
@@ -1,19 +1,19 @@
 const sentence = "hello there from lighthouse labs";
-let num = 0
-let n = sentence.length * 50 +10
+let num = 0;
+let n = sentence.length * 50 + 10;
 
 const typewriter = function(sentence) {
-	for (const char of sentence) {
-		num += 50
-		setTimeout(() => {
-  		process.stdout.write(char)
-		}, num) 
-	}
-	setTimeout(() => {
-  		process.stdout.write('\n')
-		}, n) 
+  for (const char of sentence) {
+    num += 50;
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, num);
+  }
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, n);
 };
-console.log(typewriter(sentence))
+console.log(typewriter(sentence));
 
 
 
